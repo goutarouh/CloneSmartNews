278b55bd9a899416c9184d625277b853a6f99971
goutarouh <gouta.gorogoro@gmail.com>
2021-07-27 22:31:57 +0900

    ニュースのジャンルバー完成

--- app/src/main/java/com/github/goutarouh/clonesmartnews/MainActivity.kt
+++ app/src/main/java/com/github/goutarouh/clonesmartnews/MainActivity.kt
@@ -4,20 +4,23 @@ import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
-import com.github.goutarouh.clonesmartnews.ui.home.NewsListScreen
 import com.github.goutarouh.clonesmartnews.ui.theme.CloneSmartNewsTheme
 
 class MainActivity : ComponentActivity() {
@@ -33,11 +36,17 @@ class MainActivity : ComponentActivity() {
 }
 @Composable
 fun PreviewNewsList() {
-    Button(
-        onClick = {},
-        modifier = Modifier.border(1.dp, Color.Red).size(100.dp),
-        colors = ButtonDefaults.buttonColors(backgroundColor = Color.Blue)
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
     ) {
-        Text(text = "button", modifier = Modifier.border(1.dp, Color.Green).fillMaxSize())
+        Text(
+            text = "hello jetpack compose",
+            modifier = Modifier
+                .background(color = Color.Gray, shape = RoundedCornerShape(100.dp))
+                .width(250.dp).height(400.dp)
+                .wrapContentSize(align = Alignment.Center)
+        )
     }
 }
--- app/src/main/java/com/github/goutarouh/clonesmartnews/ui/home/news/SmartNewsTabs.kt
+++ app/src/main/java/com/github/goutarouh/clonesmartnews/ui/home/news/SmartNewsTabs.kt
@@ -1,12 +1,16 @@
 package com.github.goutarouh.clonesmartnews.ui.home.news
 
 import android.util.Log
+import android.view.Gravity
 import androidx.compose.animation.core.FastOutSlowInEasing
 import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.animation.core.tween
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.material.TabRowDefaults.tabIndicatorOffset
 import androidx.compose.runtime.*
@@ -16,7 +20,10 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.composed
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.graphics.drawscope.rotate
 import androidx.compose.ui.platform.debugInspectorInfo
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 
@@ -66,14 +73,25 @@ fun SmartNewsTabs(
                     .background(color = tabColorList[selectedTabIndexState.value % (tabColorList.size)])
             )
         },
+        backgroundColor = Color.Black,
+        modifier = Modifier.height(45.dp),
         tabs = {
             genreList.forEachIndexed { index, genre ->
-                Button(
-                    onClick = { selectedTabIndexState.value = index },
-                    colors = ButtonDefaults.buttonColors(backgroundColor = tabColorList[index % (tabColorList.size)])
+                Box(
+                    modifier = Modifier
+                        .fillMaxHeight()
+                        .wrapContentSize(Alignment.BottomCenter)
                 ) {
                     Text(
                         text = genre.name,
+                        color = Color.White,
+                        modifier = Modifier
+                            .background(color = tabColorList[index % (tabColorList.size)], shape = RoundedCornerShape(topStart = 4.dp, topEnd = 4.dp))
+                            .clickable { selectedTabIndexState.value = index }
+                            .height(if (index == selectedTabIndexState.value) 45.dp else 40.dp)
+                            .padding(8.dp)
+                            .widthIn(min = 80.dp)
+                            .wrapContentSize(align = Alignment.Center)
                     )
                 }
             }
